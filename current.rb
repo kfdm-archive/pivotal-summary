c4f5950f7baa60138587bc4d724f8330e70cfc5a
Paul Traylor <paul@wegame.com>
2011-02-18 10:38:52 -0800

    Add colors for ticket types Ignore accepted for current list

--- current.rb
+++ current.rb
@@ -14,20 +14,37 @@ require 'colored'
 
 @token = `defaults read com.pivotaltracker token`
 @owner = `defaults read com.pivotaltracker name`
-@state = ['unstarted', 'started', 'finished', 'delivered', 'accepted', 'rejected']
+@state = ['unstarted', 'started', 'finished', 'delivered', 'rejected']
 @today = Time.now.strftime("%m/%d/%Y")
 
 def format_state(str)
 	case(str)
 	when 'started'
 		return "#{str}".yellow
-	when 'accepted'
-		return "#{str}".green
+	when 'rejected'
+		return "#{str}".red
+	when 'finished','delivered'
+	  return "#{str}".green
 	when 'unstarted'
 		return "#{str}".blue
 	else
 		return str
 	end
+end
+
+def format_type(str)
+  case(str)
+  when 'feature'
+    return "#{str}".yellow
+  when 'chore'
+    return "#{str}".blue
+  when 'bug'
+    return "#{str}".red
+  when 'release'
+    return "#{str}".green
+  else
+    return str
+  end
 end
 
 puts "Printing tickets for #{@today.cyan}"
@@ -38,14 +55,14 @@ PivotalTracker::Client.token = @token
 @projects.each do |project|
 	begin
 		@a_project = PivotalTracker::Project.find(project.id)
-		puts project.name.yellow.bold
+		puts "#{project.name.yellow.bold} - https://www.pivotaltracker.com/projects/#{project.id}"
 		stories = @a_project.stories.all(
 				:owner => @owner,
 				:state => @state
 			)
 		
 		stories.each do |story|
-			puts "#{story.name} - #{format_state(story.current_state)} - #{story.estimate}"
+			puts "#{story.name} - #{format_state(story.current_state)} - #{format_type(story.story_type)} - #{story.estimate}"
 			puts story.url
 			puts
 		end
